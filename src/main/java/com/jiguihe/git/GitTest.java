fa76ffa7b1cf6f6da7e52d20d2a5353967a1cc0c
jiguihe <jiguihede@163.com>
2022-01-01 17:23:18 +0800

    first commit

---
+++ src/main/java/com/jiguihe/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.jiguihe.git;
+/**
+ *@Description 
+ *@author Heguiji
+ *@version 1.0
+ *@date 2022/1/1 17:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
